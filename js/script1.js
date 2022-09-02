fe3ed7e2c69ea05cfd580abb5b28e4f725b00199
Murshid Bin Masud Sourov <61899629+mbmSouRov@users.noreply.github.com>
2022-09-02 21:11:45 +0600

    catagory-api added

--- js/script1.js
+++ js/script1.js
@@ -1,1 +1,24 @@
-console.log("GGWP");
+const catagorySection = document.getElementById("catagory-section");
+
+const showCatagory = () => {
+  const url1 = "https://openapi.programming-hero.com/api/news/categories";
+  fetch(url1)
+    .then((res) => res.json())
+    .then((datas) => showData(datas.data.news_category));
+
+  showData = (datas) => {
+    datas.forEach((data) => {
+      console.log(data.category_name);
+      const newDiv = document.createElement("li");
+      newDiv.innerHTML = `
+    <a
+                href="#"
+                class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-black dark:hover:bg-gray-700 dark:hover:text-red md:dark:hover:bg-transparent"
+                >${data.category_name}</a
+              >
+    `;
+      catagorySection.appendChild(newDiv);
+    });
+  };
+};
+showCatagory();

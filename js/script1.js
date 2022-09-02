f6704aba313e5bd48178d0baec539a133c00a78c
Murshid Bin Masud Sourov <61899629+mbmSouRov@users.noreply.github.com>
2022-09-02 23:06:12 +0600

    no of article section done

--- js/script1.js
+++ js/script1.js
@@ -1,4 +1,5 @@
 const catagorySection = document.getElementById("catagory-section");
+const sectionItemsFound = document.getElementById("section-itemsFound");
 
 const showCatagory = () => {
   const url1 = "https://openapi.programming-hero.com/api/news/categories";
@@ -8,12 +9,13 @@ const showCatagory = () => {
 
   showData = (datas) => {
     datas.forEach((data) => {
-      console.log(data.category_name);
+      //   console.log(data);
       const newDiv = document.createElement("li");
       newDiv.innerHTML = `
     <a
-                href="#"
-                class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-black dark:hover:bg-gray-700 dark:hover:text-red md:dark:hover:bg-transparent"
+                style="cursor: pointer"
+                onclick="showNews('${data.category_id}','${data.category_name}')"
+                class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-red-900 dark:hover:bg-gray-700 dark:hover:text-red md:dark:hover:bg-transparent"
                 >${data.category_name}</a
               >
     `;
@@ -21,4 +23,28 @@ const showCatagory = () => {
     });
   };
 };
+const showNews = (catagoryNo, catagoryName) => {
+  const url2 = `https://openapi.programming-hero.com/api/news/category/${catagoryNo}`;
+  fetch(url2)
+    .then((res) => res.json())
+    .then((newss) => showNewsDetails(newss));
+
+  showNewsDetails = (allNews) => {
+    const newss = allNews.data;
+    newss.forEach((news) => {
+      if (allNews.status != true) {
+        sectionItemsFound.innerHTML = `
+          0 items found for category ${catagoryName}
+          `;
+      } else {
+        sectionItemsFound.innerHTML = `
+          ${newss.length} items found for category ${catagoryName}
+          `;
+      }
+
+      console.log(news);
+    });
+  };
+};
+
 showCatagory();

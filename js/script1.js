d61b6c90ec5643a6d6d08a72b6a37045bffb1b86
Murshid Bin Masud Sourov <61899629+mbmSouRov@users.noreply.github.com>
2022-09-03 16:57:53 +0600

    almost 90% done

--- js/script1.js
+++ js/script1.js
@@ -2,7 +2,9 @@ const catagorySection = document.getElementById("catagory-section");
 const sectionItemsFound = document.getElementById("section-itemsFound");
 const newsArticle = document.getElementById("news-article");
 const spinner = document.getElementById("spinner");
-
+const modalTitle = document.getElementById("modal-title");
+const authorName = document.getElementById("author-name");
+const viewCount = document.getElementById("view-count");
 const showSpinner = (tf) => {
   if (tf === true) {
     spinner.classList.remove("hidden");
@@ -51,6 +53,7 @@ const showNews = (catagoryNo, catagoryName) => {
           `;
     newss.forEach((news) => {
       const newDiv1 = document.createElement("div");
+      console.log(news._id);
       if (news.author.published_date === null) {
         news.author.published_date = `Date Not Found`;
       }
@@ -111,79 +114,14 @@ const showNews = (catagoryNo, catagoryName) => {
               <div>
               <!-- Modal toggle -->
               <button
-                class="block text-red-900 border bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-1 text-center dark:bg-white dark:hover:text-white dark:hover:bg-red-900"
-                type="button"
-                data-modal-toggle="modal"
-              >
-                See More!
-              </button>
-
-              <!-- Main modal -->
-              <div
-                aria-hidden="true"
-                class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center"
-                id="modal"
-                tabindex="-1"
-                aria-modal="false"
-              >
-                <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
-                  <!-- Modal content -->
-                  <div
-                    class="relative bg-white rounded-lg shadow dark:bg-gray-700"
-                  >
-                    <!-- Modal header -->
-                    <div
-                      class="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600"
-                    >
-                      <h3
-                        class="text-xl font-semibold text-gray-900 dark:text-white"
-                      >
-                        Terms of Service
-                      </h3>
-                      <button
-                        type="button"
-                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                        data-modal-toggle="defaultModal"
-                      >
-                        <svg
-                          aria-hidden="true"
-                          class="w-5 h-5"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                            clip-rule="evenodd"
-                          ></path>
-                        </svg>
-                      </button>
-                    </div>
-                    <!-- Modal body -->
-                    <div class="p-6 space-y-6">
-                      <p
-                        class="text-base leading-relaxed text-gray-500 dark:text-gray-400"
-                      >
-                        With less than a month to go before the European Union
-                        enacts new consumer privacy laws for its citizens,
-                        companies around the world are updating their terms of
-                        service agreements to comply.
-                      </p>
-                      <p
-                        class="text-base leading-relaxed text-gray-500 dark:text-gray-400"
-                      >
-                        The European Union's General Data Protection
-                        Regulation (G.D.P.R.) goes into effect on May 25 and
-                        is meant to ensure a common set of data rights in the
-                        European Union. It requires organizations to notify
-                        users as soon as possible of high-risk data breaches
-                        that could personally affect them.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
+                  onclick="showInformation('${news._id}')"
+                  type="button"
+                  class="px-4 py-1.5 bg-red-900 text-white font-medium text-xs leading-tight rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700"
+                  data-bs-toggle="modal"
+                  data-bs-target="#exampleModal"
+                >
+                  See More!
+                </button>
             </div>
             </div>
           </div>
@@ -192,6 +130,25 @@ const showNews = (catagoryNo, catagoryName) => {
       newsArticle.appendChild(newDiv1);
       showSpinner(false);
     });
+  };
+};
+
+const showInformation = (id) => {
+  const url3 = `https://openapi.programming-hero.com/api/news/${id}`;
+  fetch(url3)
+    .then((res) => res.json())
+    .then((datas) => showInfos(datas.data[0]));
+
+  const showInfos = (data) => {
+    if (data.total_view === null) {
+      data.total_view = `No Info`;
+    }
+    if (data.author.name === null) {
+      data.author.name = `Author Not Found`;
+    }
+    modalTitle.innerText = `${data.title}`;
+    authorName.innerText = `${data.author.name}`;
+    viewCount.innerText = `${data.total_view}`;
   };
 };
 
